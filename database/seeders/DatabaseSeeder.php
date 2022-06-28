232e1f6e8315e7b62e6826674b904df78a4a1219
Flavio Huanacchiri <flavio.huanacchiri@gmail.com>
2022-06-27 23:23:01 -0500

    Fix: Updat Default seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Schema::disableForeignKeyConstraints();
+
+        DB::table("track")->truncate();
+        DB::table("album")->truncate();
+        DB::table("artist")->truncate();
+       
+        $this->call(ArtistSeeder::class);
+        $this->call(AlbumSeeder::class);
+        $this->call(TrackSeeder::class);
+
+        Schema::enableForeignKeyConstraints();
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
